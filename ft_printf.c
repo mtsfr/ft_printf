0767417d7a1fcdb803ca674700b331fb84c5879c
Matheus Faria Borges Da Silva <mfaria-b@c2r6s1.42.rio>
2023-11-06 23:04:25 -0300

    update 06.11

--- ft_printf.c
+++ ft_printf.c
@@ -6,26 +6,57 @@
 /*   By: mfaria-b <mfaria-b@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 19:20:07 by mfaria-b          #+#    #+#             */
-/*   Updated: 2023/11/06 20:35:19 by mfaria-b         ###   ########.fr       */
+/*   Updated: 2023/11/06 23:01:31 by mfaria-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+static int	type_handle(const char c, va_list args)
+{
+	int	len;
+
+	len = 0;
+	if (c == 'c')
+		len += ft_putchar(va_arg(args, int));
+	else if (c == 's')
+		//
+	else if (c == 'p')
+		//
+	else if (c == 'd')
+		//
+	else if (c == 'i')
+		//
+	else if (c == 'u')
+		//
+	else if (c == 'x')
+		//
+	else if (c == 'X')
+		//
+	else if (c == '%')
+		//
+	return (len);
+}
+
 int	ft_printf(const char *str, ...)
 {
-	va_list args;
 	int		i;
+	int		len;
+	va_list	args;
 	
 	i = 0;
+	len = 0;
 	va_start(args, str);
 
 	while(str[i])
 	{
-		if (str[i] == '%' && ft_strchr("cspdiuxX%", str[i + 1]))
+		if (str[i] == '%') // && ft_strchr("cspdiuxX%", str[i + 1]) 
 		{
-			
+			len += type_handle((str[i + 1]), args);
 		}
+		else
+			len += ft_putchar(str[i]);
 		i++;
 	}
 
 	va_end(args);
+	return (len);
 }
